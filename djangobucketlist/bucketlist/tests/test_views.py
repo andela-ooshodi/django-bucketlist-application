b104d856325a99181e8b71dfdfd1ae76191035f3
olufunmiladeo <tyos137@gmail.com>
2015-11-08 23:16:09 +0100

    (fixes) wrote end-to-end testing for bucketlist app

--- djangobucketlist/bucketlist/tests/test_views.py
+++ djangobucketlist/bucketlist/tests/test_views.py
@@ -5,20 +5,65 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class AuthenticationTestCase(StaticLiveServerTestCase):
+class BucketAppFunctionalityTestCase(StaticLiveServerTestCase):
     fixtures = ['users.json']
 
     def setUp(self):
-        self.browser = webdriver.PhantomJS()
+        self.browser = webdriver.Firefox()
         self.browser.set_window_size(1400, 1000)
         self.browser.implicitly_wait(10)
 
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_reach_index_page(self):
+    def test_bucketlistapp(self):
         self.browser.get(self.live_server_url)
 
         # asserting index page was successfully reached
         body = self.browser.find_element_by_tag_name('body')
-        self.assertIn('myBucketlist', body.text)
+        self.assertIn('Welcome to myBucketlist', body.text)
+
+        # asserting a successful login
+        self.browser.find_element_by_xpath(
+            "//button[contains(text(),'Get Started')]").click()
+        self.browser.find_element_by_name('username').send_keys('laddeos')
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('laddeos')
+        password_field.send_keys(Keys.RETURN)
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('My bucketlist', body.text)
+
+        # add a new bucketlist
+        self.browser.find_element_by_id('add-list-icon').click()
+        self.browser.find_element_by_name(
+            'name').send_keys('A new bucketlist')
+        self.browser.find_element_by_xpath(
+            "//button[contains(text(),'Add')]").click()
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Items', body.text)
+
+        # add a new bucketitem
+        self.browser.find_element_by_id('add-item-icon').click()
+        self.browser.find_element_by_name(
+            'name').send_keys('A new bucketitem')
+        self.browser.find_element_by_xpath(
+            "//button[contains(text(),'Add')]").click()
+        self.browser.refresh()
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('A new bucketitem', body.text)
+
+        # delete bucketitem
+        self.browser.find_element_by_class_name('delete-item').click()
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertNotIn('A new bucketitem', body.text)
+
+        # delete bucketlist
+        self.browser.find_element_by_id('back').click()
+        self.browser.find_element_by_class_name('delete-list').click()
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertNotIn('A new bucketlist', body.text)
+
+        # assert successful logout
+        self.browser.find_element_by_id('logout').click()
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Welcome to myBucketlist', body.text)

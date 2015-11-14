642461dd19a46ed8635d7f3979588ef980211cc2
olufunmiladeo <tyos137@gmail.com>
2015-11-14 22:08:20 +0100

    (fix) Added secret key in base.py

--- djangobucketlist/djangobucketlist/settings/base.py
+++ djangobucketlist/djangobucketlist/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'm(%x1m*2!qs9(l(s&n0nft&$9%3dbpcrc_v#*3cxd7#thj0zbb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

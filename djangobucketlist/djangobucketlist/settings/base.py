6ff900fa3b7eabb38a18ce55927552dfeb1c433a
olufunmiladeo <tyos137@gmail.com>
2015-11-17 15:05:51 +0100

    (fix) created custom django message tags

--- djangobucketlist/djangobucketlist/settings/base.py
+++ djangobucketlist/djangobucketlist/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.contrib.messages import constants as message_constants
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -111,6 +112,12 @@ STATICFILES_FINDERS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# custom message tag for django messaging middleware
+MESSAGE_TAGS = {
+    message_constants.ERROR: 'danger'
+}
+
 
 # Django REST_FRAMEWORK global settings
 

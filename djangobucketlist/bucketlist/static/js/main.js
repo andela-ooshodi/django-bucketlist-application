76eaedfbd9e1ae4b9f866bf1771b1266d2405b4e
olufunmiladeo <tyos137@gmail.com>
2015-12-31 11:11:08 +0100

    [Feature] Restyled the index page

--- djangobucketlist/bucketlist/static/js/main.js
+++ djangobucketlist/bucketlist/static/js/main.js
@@ -1,23 +1,27 @@
+var eventListeners = {
+    init: function() {
+        $("#flash-message").fadeOut(3000);
+        // Switch to registration form
+        $("#registration-link").on("click", function(e) {
+            e.preventDefault();
+            $(".login-form").hide();
+            $(".registration-form").show();
+        });
+        // Switch to login form
+        $("#login-link").on("click", function(e) {
+            e.preventDefault();
+            $(".registration-form").hide();
+            $(".login-form").show();
+        });
+        // Submit checkbox value
+        $("body").on("click", "#item-checkbox", function(e) {
+            e.preventDefault();
+            var url = $(this).attr("href");
+            window.location.assign(url);
+        });
+    }
+};
 $(document).ready(function() {
-    $('#flash-message').fadeOut(3000);
+    eventListeners.init();
     // $.material.init();
-
-    // Switch to registration form
-    $('#registration-link').on('click', function (event) {
-        event.preventDefault();
-        $('.login-form').hide();
-        $('.registration-form').show();
-    });
-    // Switch to login form
-    $('#login-link').on('click', function (event) {
-        event.preventDefault();
-        $('.registration-form').hide();
-        $('.login-form').show();
-    });
-    // Submit checkbox
-    $('body').on('click', '#item-checkbox', function (event) {
-        event.preventDefault();
-        var url = $(this).attr('href');
-        window.location.assign(url);
-    });
 });
